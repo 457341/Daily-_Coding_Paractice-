09681703eafc924e17c037992cd63defa0b3a24d
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-03-22 23:47:13 +0300

    Program How to get a calender of given valuse of year and month from user

---
+++ prog12.py
@@ -0,0 +1,10 @@
+# Monday 22 March 2021
+# 12. Write a Python program to print the calendar of a given month and year.
+# Note : Use 'calendar' module.
+import calendar
+y = int(input("Enter the year: "))
+m = int(input("Enter the month: "))
+c = calendar.month(y,m)
+print(calendar.MONDAY)
+print(calendar.Calendar)
+print(c)
