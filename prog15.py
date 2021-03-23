b2078331dafd69e0a648a0f44ca9667831a2bf61
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-03-23 23:55:17 +0300

    Python program to get the volume of a sphere

---
+++ prog15.py
@@ -0,0 +1,8 @@
+# Write a Python program to get the volume of a sphere with radius 6
+
+# Formula: V=4/3πr3
+
+import math
+radius = 6
+volume = 4/(3*math.pi*radius*radius*radius)
+print("Volume of a sphere is: ",volume)
