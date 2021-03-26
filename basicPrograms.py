07f68fc2f520e3dd78b940aaa6c571084eb45dc4
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-03-26 23:31:56 +0300

    program to display your details like name, age, address in three different lines.

---
+++ basicPrograms.py
@@ -0,0 +1,7 @@
+# Write a Python program to display your details like name, age, address in three different lines.
+name = "Manzoor"
+surname = "Hussain"
+
+country = "Pakistan"
+print("Name: {}\nSurname: {}\nCountry: {}".format(name,surname,country))
+
