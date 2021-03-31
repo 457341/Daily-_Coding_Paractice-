8d46a08014afd239f475cf969aa8b1bbddbe3bb8
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-03-31 20:25:38 +0300

    program to remove the first item from a specified list.

--- basicPrograms.py
+++ basicPrograms.py
@@ -57,3 +57,11 @@ def swap(x,y):
     print("New valuees are ",x,y)
     
 swap(x,y)
+
+
+# Write a Python program to remove the first item from a specified list.
+color = ["Red", "Black", "Green", "White", "Orange"]
+print("\nOriginal Color: ",color)
+del color[0]
+print("After removing the first color: ",color)
+print()
