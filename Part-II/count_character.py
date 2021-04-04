3b73d44367e6417d59675ea8a13a3e9e8761716f
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-04-04 20:45:09 +0300

    How to read a file from the user

---
+++ Part-II/count_character.py
@@ -0,0 +1,22 @@
+def count_character(file):
+
+    print()
+# import os
+
+# file1 = open("\\abc.txt","r")
+# for i in file1:
+#     i.split()
+#     print(i.read())
+
+
+# Python code to illustrate with()
+# with open("abc.txt") as file:  
+#     data = file.read() 
+# import os
+# file1 = open(os.path.expanduser('abc.txt',r))
+# print(file1.read())
+
+# fileName= input("Please enter the name of the file you'd like to use: ")
+file = open('abc.txt', 'r')  #Opens file
+print((file.read()))
+
