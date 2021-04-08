516f6323d756b89cf03efdd34e80d31855ba9c5d
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-04-08 19:49:12 +0300

    conconating of adjective, and verbs etc

---
+++ 12 Python Projects/madlibs.py
@@ -0,0 +1,18 @@
+# Concating of string
+# Differnet types of concanating
+# name = "manzoor"
+# print("My name is "+name)
+# print("My name is ",name)
+# print("My name is {}".format(name))
+# print(f"My name is {name}")
+
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
