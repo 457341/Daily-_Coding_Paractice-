cbce96dfbf0a87dc0330291e18d3f0c5e414b678
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-04-08 20:39:50 +0300

    If the  number you guessed in your mind is low or high , wite l, h or c for correct

--- 12 Python Projects/guess_number.py
+++ 12 Python Projects/guess_number.py
@@ -11,4 +11,22 @@ def guess(x):
         elif random_number < guess:
             print("Number is too low, try again.")
     print(f"Congratulations you guessed the number {random_number} correctly.")
-guess(10)
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if high!=low:
+            guess = random.randint(low,high)
+        else:
+            guess = low
+        feedback = input(f'Is {guess} too high (H), too low (L) or correct (C)?? ').lower()
+        if feedback =='h':
+            high = guess -1
+        elif feedback == 'l':
+            low = guess + 1
+        
+    print(f"Yay! The computer guessed your number, {guess}, correctly")
+# guess(10)
+computer_guess(10)
