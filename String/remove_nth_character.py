14eb8156f8b4a298603b01adf042422fd9038c2d
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-04-16 20:15:47 +0300

    A new approach to delete nth index from non-empty string

--- String/remove_nth_character.py
+++ String/remove_nth_character.py
@@ -7,5 +7,11 @@ def remove_char(string,n):
     string = ''.join(string)
     print(string)
 
+def remove_char2(string,n):
+    first_part = string[:n]
+    second_part = string[n+1:]
+
+    print(f'The result of {string} after {n}th index deleting is: {first_part + second_part}')
 
 remove_char("Manzoor",1)
+remove_char2("Manzoor",5)
