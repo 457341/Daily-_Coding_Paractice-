fcc7afdd03d0b2197ac52e9473c5ffcd87f35cdb
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-04-18 00:19:18 +0300

    More effective solution added , without using two for loops

--- String/each_word.py
+++ String/each_word.py
@@ -1,10 +1,10 @@
 # Write a Python program to count the occurrences of each word in a given sentence
-def count_each_word():
-    string = "My name is manzoor is, and i am from Pakistan. one two one four the it hello you the from"
+def count_each_word(string):
+    
     string = string.split(' ')
     string = list(string)
     count_word = {}
-    print(string)
+    # print(string)
     for i in string:
         count = 0
         for j in range(len(string)):
@@ -15,4 +15,21 @@ def count_each_word():
         count = 0
     print(count_word)
 
-count_each_word()
+def count_each_word2(string): # a better solution for upper function
+    string = string.split(' ')
+    count_word = {}
+    # print(string)
+    for i in string:
+        # count = 0
+        if i in count_word:
+            count_word[i] += 1
+        else:
+            count_word[i] = 1
+        
+    print(count_word)
+string1 = "My name is manzoor is, and i am from Pakistan. one two one four the it hello you the from"
+string2 = "One two One four five six two Five"
+count_each_word(string1)
+count_each_word(string2)
+count_each_word2(string1)
+count_each_word2(string2)
