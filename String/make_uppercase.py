13a4b39ca7d00db32589286514707fa52c7b2f08
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-04-23 00:19:15 +0300

    Complete solution added

--- String/make_uppercase.py
+++ String/make_uppercase.py
@@ -1,7 +1,18 @@
 #  Write a Python function to convert a given string to all uppercase if
 #   it contains at least 2 uppercase characters in the first 4 characters.
 def make_uppercase(string):
-    string = string.split(',')
+    # string = string.split()
+    string = list(string)
+    count = 0
+    for i in string[:4]:
+        if i == (i).upper():
+            count = count + 1
+        string = ''.join(string)
+        if count == 4:
+            string = string.upper()
     print(string)
 
-make_uppercase("ManzooR")
+make_uppercase("Manzoor")
+make_uppercase("PYTHOn")
+make_uppercase("PROGramming")
+# print("Manzoor".upper())
