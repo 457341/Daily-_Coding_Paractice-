250b2e6981b1ea6d5748b85f1452a16ff3631b4b
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-04-22 23:56:30 +0300

    function to convert a given string to all uppercase if it contains at least 2 uppercase characters in the first 4 characters.

---
+++ String/make_uppercase.py
@@ -0,0 +1,7 @@
+#  Write a Python function to convert a given string to all uppercase if
+#   it contains at least 2 uppercase characters in the first 4 characters.
+def make_uppercase(string):
+    string = string.split(',')
+    print(string)
+
+make_uppercase("ManzooR")
