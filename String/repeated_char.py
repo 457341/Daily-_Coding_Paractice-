f53a9020739610e24cbe5338d17240fb6cc729be
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-04-25 23:33:10 +0300

    Complete solution of repated characters in string

--- String/repeated_char.py
+++ String/repeated_char.py
@@ -1,20 +1,29 @@
 # Write a Python program to count repeated characters in a string. 
 # Sample string: 'thequickbrownfoxjumpsoverthelazydog'
 # Expected output :
+# o 4
+# e 3
+# u 2
+# h 2
+# r 2
+# t 2
 
 
 def count_repeated_char(string):
 
     count_repeated = {}
-    string = list(string)
+    # string = list(string)
     for i in string:
-        if i in string:
-            count_repeated[i] = count_repeated[i] + 1
-        else:
-            count_repeated[i] = 1
+        if string.count(i) >1:
+            count_repeated[i] = string.count(i)
+        # print("Element: ",i,"= ",count)
 
-    print(count_repeated)
+    for i in count_repeated:
+        print(f'{i}: {count_repeated[i]}') 
+
 
 
 string = 'thequickbrownfoxjumpsoverthelazydog'
+# string2 = "manzoorhussain"
 count_repeated_char(string)
+# print(string2.count('a'))
