88005cb058edb45e3937fd3890b541a9e14fba1d
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-04-27 20:46:27 +0300

    program to check first non repeated character in a string

--- String/count_character.py
+++ String/count_character.py
@@ -6,3 +6,18 @@ string_dic = {}
 for i in simple_string:
     string_dic[i] = simple_string.count(i)
 print(string_dic)
+
+
+def firstNonRepeated(string):
+    non_repeated = []
+    for i in string:
+        if string.count(i) == 1:
+            non_repeated.append(i)
+
+    if not non_repeated:
+        non_repeated.append('None')
+    
+    print(f'First non repeated character: {non_repeated[0]}')
+firstNonRepeated('abcdef')
+firstNonRepeated('abcabcdef')
+firstNonRepeated('aabbcc')
