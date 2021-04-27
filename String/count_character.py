ce3ff4da7f40c80b57a71d267637ae4795fbbe91
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-04-27 20:53:18 +0300

    program to check first  repeated character in a string

--- String/count_character.py
+++ String/count_character.py
@@ -1,23 +1,38 @@
 # Write a Python program to count the number of characters (character frequency) in a string. 
 # Sample String : google.com'
 # Expected Result : {'g': 2, 'o': 3, 'l': 1, 'e': 1, '.': 1, 'c': 1, 'm': 1}
-simple_string = input("Enter string: ")
-string_dic = {}
-for i in simple_string:
-    string_dic[i] = simple_string.count(i)
-print(string_dic)
+# simple_string = input("Enter string: ")
+# string_dic = {}
+# for i in simple_string:
+#     string_dic[i] = simple_string.count(i)
+# print(string_dic)
 
 
-def firstNonRepeated(string):
-    non_repeated = []
-    for i in string:
-        if string.count(i) == 1:
-            non_repeated.append(i)
+# def firstNonRepeated(string):
+#     non_repeated = []
+#     for i in string:
+#         if string.count(i) == 1:
+#             non_repeated.append(i)
 
-    if not non_repeated:
-        non_repeated.append('None')
+#     if not non_repeated:
+#         non_repeated.append('None')
     
-    print(f'First non repeated character: {non_repeated[0]}')
-firstNonRepeated('abcdef')
-firstNonRepeated('abcabcdef')
-firstNonRepeated('aabbcc')
+#     print(f'First non repeated character: {non_repeated[0]}')
+# firstNonRepeated('abcdef')
+# firstNonRepeated('abcabcdef')
+# firstNonRepeated('aabbcc')
+# Write a Python program to find the first repeated character in a given string.
+
+def firstRepeated(string):
+    repeated = []
+    for i in string:
+        if string.count(i) >1:
+            repeated.append(i)
+
+
+    if not repeated:
+        repeated.append('None')
+    print(f'First  repeated character: {repeated[0]}')
+firstRepeated('abcdef')
+firstRepeated('abcabcdef')
+firstRepeated('aabbcc')
