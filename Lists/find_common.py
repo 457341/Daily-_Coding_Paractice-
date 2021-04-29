fbbe9c78da5e678d957a850eccb36ca9f6febf60
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-04-29 23:58:37 +0300

    Not completed yet

---
+++ Lists/find_common.py
@@ -0,0 +1,7 @@
+# Write a Python function that takes two lists and returns True if they have at least one common member
+def find_common_member(list1,list2):
+    
+    for i in 
+
+print(find_common_member([1,2,3,4,5], [5,6,7,8,9]))
+print(find_common_member([1,2,3,4,5], [6,7,8,9]))
