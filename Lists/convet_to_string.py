d9bffdc068851ecbd8770098e4bd394c0d0004d1
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-04-30 23:17:46 +0300

    program to find the index of an item in a specified list

--- Lists/convet_to_string.py
+++ Lists/convet_to_string.py
@@ -2,3 +2,9 @@
 def convert_to_string(list):
     print(''.join(list))
 convert_to_string(['m','a','n','z','o','o','r'])
+
+# Write a Python program to find the index of an item in a specified list
+def foo(list):
+    for key,value in enumerate(list):
+        print(key,value)
+foo(['h','u','s','a','i','n'])
