6762d2984b0b6fe10ca13bf03dfc7b2b5e4c6d55
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-05-01 19:14:40 +0300

    program to append a list to the second list

---
+++ Lists/append_list.py
@@ -0,0 +1,16 @@
+# Write a Python program to append a list to the second list
+def append_list(list1,list2):
+    print("")
+
+    # list1.append(list2)
+    # print(list1)
+    for i in list2:
+        list1.append(i)
+    print(list1)
+list1 = [1,2,3,4,5]
+list2 = [6,7,8,9]
+# append_list(list1,list2)
+
+
+# another solution is below:
+print(list1+list2)
