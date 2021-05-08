4261d84e35b229a6b543e6b1121fcf7fb97786e4
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-05-08 23:17:31 +0300

    Don't half part

--- Lists/remove_consective.py
+++ Lists/remove_consective.py
@@ -4,17 +4,25 @@
 # After removing consecutive duplicates:
 # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 4]
 
+# def remove_duplicate(list):
+#     new_list = []
+#     j = 0
+#     for i in range(len(list)):
+#         if list[j] == list[j+1]:
+#             print(list[j])
+#             # new_list.append(list[i])
+#             j += 1  
+            
+#     # print(new_list)
+
 def remove_duplicate(list):
     new_list = []
-    j = 0
+    # print(len(list))
     for i in range(len(list)):
-        if list[j] == list[j+1]:
-            print(list[j])
-            # new_list.append(list[i])
-            j += 1
-            
-    # print(new_list)
-
+        if  i<len(list)-1:
+            if list[i] == list[i+1]:
+                new_list.append(list[i])            
+    print(new_list)#[0, 4, 6, 6, 4]
 
 
 list =[0, 0, 1, 2, 3, 4, 4, 5, 6, 6, 6, 7, 8, 9, 4, 4]
