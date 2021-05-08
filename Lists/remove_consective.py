9c4626a6380133be38441fdec20f652e9d93c5f4
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-05-09 00:09:51 +0300

    Used built in function to solve completely this problem

--- Lists/remove_consective.py
+++ Lists/remove_consective.py
@@ -1,25 +1,12 @@
 # 73. Write a Python program to remove consecutive duplicates of a given list.
-# Original list:
-# [0, 0, 1, 2, 3, 4, 4, 5, 6, 6, 6, 7, 8, 9, 4, 4]
-# After removing consecutive duplicates:
-# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 4]
-
-# def remove_duplicate(list):
-#     new_list = []
-#     j = 0
-#     for i in range(len(list)):
-#         if list[j] == list[j+1]:
-#             print(list[j])
-#             # new_list.append(list[i])
-#             j += 1  
-            
+from itertools import groupby
 #     # print(new_list)
 
 def remove_duplicate(list):
     new_list = []
     # print(len(list))
     for i in range(len(list)+2):
-        if  i<len(list)-1:
+        if  i<len(list)-1 :
             if list[i] == list[i+1]:
                 continue
             else:
@@ -27,7 +14,15 @@ def remove_duplicate(list):
 
     print(new_list)#[0, 4, 6, 6, 4]
 
+def removeDuplicate(list):
+    new_list = []
+    for key,group in groupby(list):
+        new_list.append(key)
+    print(new_list)
 
 list =[0, 0, 1, 2, 3, 4, 4, 5, 6, 6, 6, 7, 8, 9, 4, 4]
-remove_duplicate(list)
+list2 = ['a','a','b','b','c','c','d','d','e','e','f','g','h']
+# remove_duplicate(list)
+removeDuplicate(list)
+removeDuplicate(list2)
         
