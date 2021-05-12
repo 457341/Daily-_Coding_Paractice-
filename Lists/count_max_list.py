03fe944023e708bc75e7b8c6b98c64af0dee245d
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-05-12 23:56:10 +0300

    count max list

---
+++ Lists/count_max_list.py
@@ -0,0 +1,19 @@
+# 91. Write a Python program to find the list with maximum and minimum length. Go to the editor
+# Original list:
+# [[0], [1, 3], [5, 7], [9, 11], [13, 15, 17]]
+# List with maximum length of lists:
+# (3, [13, 15, 17])
+# List with minimum length of lists:
+# (1, [0])
+# Original list:
+# [[0], [1, 3], [5, 7], [9, 11], [3, 5, 7]]
+# List with maximum length of lists:
+# (3, [3, 5, 7])
+# List with minimum length of lists:
+# (1, [0])
+# Original list:
+# [[12], [1, 3], [1, 34, 5, 7], [9, 11], [3, 5, 7]]
+# List with maximum length of lists:
+# (4, [1, 34, 5, 7])
+# List with minimum length of lists:
+# (1, [12])
