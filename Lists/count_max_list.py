a0474e59519513437467f538b1d3a45bbe143a48
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-05-13 23:10:18 +0300

    Very simple and effective solution added

--- Lists/count_max_list.py
+++ Lists/count_max_list.py
@@ -17,3 +17,40 @@
 # (4, [1, 34, 5, 7])
 # List with minimum length of lists:
 # (1, [12])
+def count_max_list(list):
+    min_length = 1
+    max_length = 1
+    new_max_list = []
+    new_min_list = []
+    for key,value in enumerate(list):
+        if len(value)>max_length:
+            max_length = len(value)
+            new_max_list = value
+        if len(value)<min_length:
+            min_length = len(value)
+            new_min_list = value
+
+    # return [value for key,value in enumerate(list) if max(len(value))]
+    print("Max length and list",max_length,new_max_list)
+    print("min length and list",min_length,new_min_list)
+    # print(max(len(list)))
+list1 =[[0], [1, 3], [5, 7], [9, 11], [13, 15, 17]]
+# count_max_list(list1)
+
+# very simple solutions
+def max_length_list(list):
+    max_length = max((len(x) for x in list))
+    max_list = max(list,key = len)
+    return (max_length,max_list)
+def min_length_list(list):
+    min_length = min((len(x) for x in list))
+    min_list = min(list,key = len)
+    return (min_length,min_list)
+
+
+
+print(max_length_list(list1))
+print(min_length_list(list1))
+# count_max_list(list)
+# count_max_list(list)
+# count_max_list(list)
