61d29b44a31f6571eb340c93f93ed14a7bda4e6e
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-06-16 18:40:16 +0300

    Question 8 Solved

---
+++ coding-challenges/beginner/Question8.py
@@ -0,0 +1,12 @@
+# Question:
+# Write a program that accepts a comma separated sequence of words as input and prints the words in a comma-separated sequence after sorting them alphabetically.
+# Suppose the following input is supplied to the program:
+# without,hello,bag,world
+# Then, the output should be:
+# bag,hello,without,world
+strr = list(input("Enter strings: ").split(","))
+print("Before sorting: ",strr)
+# strr = sorted(strr)
+strr.sort()
+print("After sorting: ",", ".join(strr))
+print(len(strr))
