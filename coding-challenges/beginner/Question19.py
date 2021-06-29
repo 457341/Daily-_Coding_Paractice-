620610c40675bf18353f523d3b76e2d147668d4f
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-06-29 23:57:56 +0300

    Tuple in ascending order

--- coding-challenges/beginner/Question19.py
+++ coding-challenges/beginner/Question19.py
@@ -15,3 +15,12 @@
 # Json,21,85
 # Then, the output of the program should be:
 # [('John', '20', '90'), ('Jony', '17', '91'), ('Jony', '17', '93'), ('Json', '21', '85'), ('Tom', '19', '80')]
+from operator import itemgetter, attrgetter
+l = []
+while True:
+    s = input()
+    if not s:
+        break
+    l.append(tuple(s.split(",")))
+
+print(sorted(l, key=itemgetter(0,1,2)))
