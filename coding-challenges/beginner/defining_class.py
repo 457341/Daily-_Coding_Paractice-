aaaff8cb6f5545589dc2dde3fb4f417996f53844
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-07-07 23:48:24 +0300

    Define a class, which have a class parameter and have a same instance parameter.

---
+++ coding-challenges/beginner/defining_class.py
@@ -0,0 +1,18 @@
+
+
+# Question:
+    # Define a class, which have a class parameter and have a same instance parameter.
+class Person:
+    # Define the class parameter "name"
+    name = "Person"
+    
+    def __init__(self, name = None):
+        # self.name is the instance parameter
+        self.name = name
+
+manzoor = Person("Manzoor")
+print("%s name is %s" % (Person.name, manzoor.name))
+
+ali = Person()
+ali.name = "Ali"
+print ("%s name is %s" % (Person.name, ali.name))
