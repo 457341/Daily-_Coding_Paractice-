a7a4e8c87e407e882f2ed7bab742df39732ea2da
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-07-14 23:48:53 +0300

    Question1 solved

---
+++ Interview_Question/question1.py
@@ -0,0 +1,18 @@
+# Suppose you are given a tuple containing ints and strings. Write a Python function to return the # of times a
+# given element, n, appears in the tuple.
+# For example, given the tuple below, where n=3, your function should return 4, since 3 appears 4 times in the
+# tuple
+# input:
+#  n = 3
+#  my_tuple = 2, 'my_string', 4, 3, 3, 3, 2, 3
+# output:
+#  4
+n = 3
+my_tuple = 2, 'my_string', 4, 3, 3, 3, 2, 3
+
+total_number = my_tuple.count(3)
+print (total_number)
+
+# Question 2 - Counting capital letters
+# Using Python, write code that will read a file and return the number of capital letters.
+# Once you have your initial piece of code, see if you can condense into a one-liner.
