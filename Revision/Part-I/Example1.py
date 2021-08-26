5255f4a3c65d6037635439e57b71df685bb44406
Manzoor Hussain <33984489+manzoorHusain@users.noreply.github.com>
2021-08-26 23:07:43 +0300

    Revision

--- Revision/Part-I/Example1.py
+++ Revision/Part-I/Example1.py
@@ -150,3 +150,16 @@ height = float(input("Input your height in Feet: "))
 weight = float(input("Input your weight in Kilogram: "))
 print("Your body mass index is: ", round(weight / (height * height), 2))
 
+def lcm(x, y):
+  if x > y:
+      z = x
+  else:
+      z = y
+  while(True):
+      if((z % x == 0) and (z % y == 0)):
+          lcm = z
+          break
+      z += 1
+  return lcm
+print(lcm(4, 6))
+print(lcm(15, 17))
