aec0bdd6e87ee79010a1f5576d5a6643925a0233
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-08-28 23:58:02 +0300

    Revision

--- Revision/Part-I/Example1.py
+++ Revision/Part-I/Example1.py
@@ -148,30 +148,32 @@
 #? Question
 #Write a Python program to calculate the sum of the digits in an integer.
 # #! Answer
-def foo(num):
-    total = 0
-    while num >= 10:
-        temp = num % 10
-        total += temp
-        num = num // 10
-    total +=num
-    print(total)
-foo(1235)
-foo(12)
-foo(100)
-foo(100045)
-foo(5245)
-def lcm(x, y):
-  if x > y:
-      z = x
-  else:
-      z = y
-  while(True):
-      if((z % x == 0) and (z % y == 0)):
-          lcm = z
-          break
-      z += 1
-  return lcm
-print(lcm(4, 6))
-print(lcm(15, 17))
+# def foo(num):
+#     total = 0
+#     while num >= 10:
+#         temp = num % 10
+#         total += temp
+#         num = num // 10
+#     total +=num
+#     print(total)
+# foo(1235)
+# foo(12)
+# foo(100)
+# foo(100045)
+# foo(5245)
+# def lcm(x, y):
+#   if x > y:
+#       z = x
+#   else:
+#       z = y
+#   while(True):
+#       if((z % x == 0) and (z % y == 0)):
+#           lcm = z
+#           break
+#       z += 1
+#   return lcm
+# print(lcm(4, 6))
+# print(lcm(15, 17))
 
+#? Question 
+#! Answer
