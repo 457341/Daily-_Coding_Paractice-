d6841531c03fe5c4927a87f65d9785959f748622
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-09-03 22:53:32 +0300

    program to count the number of each character of a given text of a text file.

--- Revision/Part-II.py
+++ Revision/Part-II.py
@@ -1,11 +1,22 @@
 # ? Question
 # Write a Python function that takes a sequence of numbers and determines whether all the numbers are different from each other.
 # ! Answer
-def foo(data):
-    if len(data) == len(set(data)):
-        return True
-    else:
-        return False
-print(foo([1,2,3]))
-print(foo([1,1,3, 4, 5, 6, 7]))
+# def foo(data):
+#     if len(data) == len(set(data)):
+#         return True
+#     else:
+#         return False
+# print(foo([1,2,3]))
+# print(foo([1,1,3, 4, 5, 6, 7]))
+#? Question
+# Write a Python program to count the number of each character of a given text of a text file.
+f = open("text.txt", "r")
+# print(f.read())
+str = "My name is manzoor"
+str = str.split()
+for s in str:
+    s = s.split()
+    print(s)
+print(str)
+#! Answer
 
