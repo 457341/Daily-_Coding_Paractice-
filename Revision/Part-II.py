c75b76fa73ad5733655300177815b2dfb55ec953
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-09-07 23:35:43 +0300

    Program to get a list of locally installed Python modules.

--- Revision/Part-II.py
+++ Revision/Part-II.py
@@ -20,64 +20,75 @@
 # print(str)
 #! Answer
 
-def remove_nums(int_list):
+# def remove_nums(int_list):
   #list starts with 0 index
-  position = 3 - 1 
-  idx = 0
-  len_list = (len(int_list))
-  while len_list>0:
-    idx = (position+idx)%len_list
-    print(int_list.pop(idx))
-    len_list -= 1
-nums = [10,20,30,40,50,60,70,80,90]
-remove_nums(nums)
+  # position = 3 - 1 
+  # idx = 0
+  # len_list = (len(int_list))
+#   while len_list>0:
+#     idx = (position+idx)%len_list
+#     print(int_list.pop(idx))
+#     len_list -= 1
+# nums = [10,20,30,40,50,60,70,80,90]
+# remove_nums(nums)
 #? Question
 # Write a Python program to find unique triplets whose three elements gives the sum of zero from an array of n integers.
 #! Answer
-def three_sum(nums):
-  result = []
-  nums.sort()
-  for i in range(len(nums)-2):
-    if i> 0 and nums[i] == nums[i-1]:
-      continue
-    l, r = i+1, len(nums)-1
-    while l < r:
-      s = nums[i] + nums[l] + nums[r]
-      if s > 0:
-        r -= 1
-      elif s < 0:
-          l += 1
-      else:
-        # found three sum
-        result.append((nums[i], nums[l], nums[r]))
-        # remove duplicates
-        while l < r and nums[l] == nums[l+1]:
-          l+=1
-          while l < r and nums[r] == nums[r-1]:
-            r -= 1
-            l += 1
-            r -= 1
-          return result
+# def three_sum(nums):
+#   result = []
+#   nums.sort()
+#   for i in range(len(nums)-2):
+#     if i> 0 and nums[i] == nums[i-1]:
+#       continue
+#     l, r = i+1, len(nums)-1
+#     while l < r:
+#       s = nums[i] + nums[l] + nums[r]
+#       if s > 0:
+#         r -= 1
+#       elif s < 0:
+#           l += 1
+#       else:
+#         # found three sum
+#         result.append((nums[i], nums[l], nums[r]))
+#         # remove duplicates
+#         while l < r and nums[l] == nums[l+1]:
+#           l+=1
+#           while l < r and nums[r] == nums[r-1]:
+#             r -= 1
+#             l += 1
+#             r -= 1
+#           return result
 
-x = [1, -6, 4, 2, -1, 2, 0, -2, 0 ]
-print(three_sum(x))
+# x = [1, -6, 4, 2, -1, 2, 0, -2, 0 ]
+# print(three_sum(x))
 #? Question
 # Write a Python program to get the top stories from Google news.
 #! Answer
-import bs4
-from bs4 import BeautifulSoup as soup
-from urllib.request import urlopen
+# import bs4
+# from bs4 import BeautifulSoup as soup
+# from urllib.request import urlopen
 
-news_url="https://news.google.com/news/rss"
-Client=urlopen(news_url)
-xml_page=Client.read()
-Client.close()
+# news_url="https://news.google.com/news/rss"
+# Client=urlopen(news_url)
+# xml_page=Client.read()
+# Client.close()
 
-soup_page=soup(xml_page,"xml")
-news_list=soup_page.findAll("item")
+# soup_page=soup(xml_page,"xml")
+# news_list=soup_page.findAll("item")
 # Print news title, url and publish date
-for news in news_list:
-  print(news.title.text)
-  print(news.link.text)
-  print(news.pubDate.text)
-  print("-"*60)
+# for news in news_list:
+#   print(news.title.text)
+#   print(news.link.text)
+#   print(news.pubDate.text)
+#   print("-"*60)
+
+
+#? Question
+# Write a Python program to get a list of locally installed Python modules.
+#! Answer
+import pkg_resources
+installed_packages = pkg_resources.working_set
+installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
+     for i in installed_packages])
+for m in installed_packages_list:
+    print(m)
