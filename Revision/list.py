6390f4a7ba526d1289a845fca7c92e498be6e060
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-09-22 23:44:20 +0300

    Some revision of list

--- Revision/list.py
+++ Revision/list.py
@@ -27,8 +27,14 @@ print("Largest number in list is: ",largest(lst))
 #? Question 
 # 4. Write a Python program to get the smallest number from a list. 
 #! Answer 
-# 
+def smallest(lst):
+    smallest_number = lst[0]
+    for i in lst:
+        if smallest_number > i:
+            smallest_number = i
+    return smallest_number
 
+print("The smallest number in list is: ",smallest(lst))
 #? Question 
 # 5. Write a Python program to count the number of strings where the string length is 2 or more 
 # and the first and last character are same from a given list of strings. 
@@ -36,3 +42,15 @@ print("Largest number in list is: ",largest(lst))
 # Expected Result : 2
 
 #! Answer 
+def check_first_and_last_characters(lst):
+    total = 0
+    if len(lst) >= 2:
+        for i in lst:
+            if i[0] == i[-1]:
+                total += 1
+    return total
+
+result = check_first_and_last_characters( ['abc', 'xyx', 'aba', '1221'])
+print("The result is: " ,result)
+# sonuc = 0
+# result2 = [sonuc +=1 :if if i[0] == i[-1]]
