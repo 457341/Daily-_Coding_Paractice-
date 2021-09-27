1e9b0ba240edc3cc8fa4b4d71fca43027593a0e6
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-09-27 23:44:44 +0300

    Previous solution commented

--- Revision/list.py
+++ Revision/list.py
@@ -1,40 +1,40 @@
 #? Question 
 # 1. Write a Python program to sum all the items in a list. 
 #! Answer 
-lst = [1, 2, 3, 4, 5, 6]
-print("Sum of list is: ",sum(lst))
+# lst = [1, 2, 3, 4, 5, 6]
+# print("Sum of list is: ",sum(lst))
 
 #? Question 
 # 2. Write a Python program to multiply all the items in a list. 
 #! Answer 
-def multiply(lst):
-    mul = 1
-    for i in lst:
-        mul *= i
-    return mul
-print("Multiplication of list is: ",multiply(lst))
+# def multiply(lst):
+#     mul = 1
+#     for i in lst:
+#         mul *= i
+#     return mul
+# print("Multiplication of list is: ",multiply(lst))
 
 #? Question 
 # 3. Write a Python program to get the largest number from a list. 
 #! Answer 
-def largest(lst):
-    largest_number = 1
-    for i in lst:
-        if largest_number < i:
-            largest_number = i
-    return largest_number
-print("Largest number in list is: ",largest(lst))
+# def largest(lst):
+#     largest_number = 1
+#     for i in lst:
+#         if largest_number < i:
+#             largest_number = i
+#     return largest_number
+# print("Largest number in list is: ",largest(lst))
 #? Question 
 # 4. Write a Python program to get the smallest number from a list. 
 #! Answer 
-def smallest(lst):
-    smallest_number = lst[0]
-    for i in lst:
-        if smallest_number > i:
-            smallest_number = i
-    return smallest_number
+# def smallest(lst):
+#     smallest_number = lst[0]
+#     for i in lst:
+#         if smallest_number > i:
+#             smallest_number = i
+#     return smallest_number
 
-print("The smallest number in list is: ",smallest(lst))
+# print("The smallest number in list is: ",smallest(lst))
 #? Question 
 # 5. Write a Python program to count the number of strings where the string length is 2 or more 
 # and the first and last character are same from a given list of strings. 
@@ -42,16 +42,16 @@ print("The smallest number in list is: ",smallest(lst))
 # Expected Result : 2
 
 #! Answer 
-def check_first_and_last_characters(lst):
-    total = 0
-    if len(lst) >= 2:
-        for i in lst:
-            if i[0] == i[-1]:
-                total += 1
-    return total
+# def check_first_and_last_characters(lst):
+#     total = 0
+#     if len(lst) >= 2:
+#         for i in lst:
+#             if i[0] == i[-1]:
+#                 total += 1
+#     return total
 
-result = check_first_and_last_characters( ['abc', 'xyx', 'aba', '1221'])
-print("The result is: " ,result)
+# result = check_first_and_last_characters( ['abc', 'xyx', 'aba', '1221'])
+# print("The result is: " ,result)
 # sonuc = 0
 # result2 = [sonuc +=1 :if if i[0] == i[-1]]
 #? Question
@@ -59,17 +59,17 @@ print("The result is: " ,result)
 # Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
 # Expected Result : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
 #! Answer
-def last_element(n):
-    return  n[-1] # Here n[1] is also fine
-def Question_6(lst):
-    # for index,i in enumerate(list):
-    #     print(i)
-    #     print(sum(i))
-    #     print(i[0])
-    return sorted(lst,key=last_element)
-q6_list = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
-print("List before sorting(according to the last element of list): ",q6_list)
-print("List after sorting(according to the last element of list): ",Question_6(q6_list))
+# def last_element(n):
+#     return  n[-1] # Here n[1] is also fine
+# def Question_6(lst):
+#     # for index,i in enumerate(list):
+#     #     print(i)
+#     #     print(sum(i))
+#     #     print(i[0])
+#     return sorted(lst,key=last_element)
+# q6_list = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+# print("List before sorting(according to the last element of list): ",q6_list)
+# print("List after sorting(according to the last element of list): ",Question_6(q6_list))
 
 # print(Question_6([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]))
 # def last(n): return n[-1]
@@ -89,25 +89,25 @@ print("List after sorting(according to the last element of list): ",Question_6(q
 # 8. Write a Python program to check a list is empty or not. 
 
 #! Answer
-def is_empty(lst):
-    return len(lst) >=1
-empty_list = []
-non_empty_list = [1, 2, 3]
-print(is_empty(empty_list))
-print(is_empty(non_empty_list))
+# def is_empty(lst):
+#     return len(lst) >=1
+# empty_list = []
+# non_empty_list = [1, 2, 3]
+# print(is_empty(empty_list))
+# print(is_empty(non_empty_list))
 #? Question
 # 9. Write a Python program to clone or copy a list. 
 
 #! Answer
 #? Question
 # 10. Write a Python program to find the list of words that are longer than n from a given list of words. 
-def find_words(lst,n):
-    new_list = []
-    for i in lst:
-        if len(i) > n:
-            new_list.append(i)
-    return new_list
-print(find_words(['Manzoor','Hussain','OMU','ODTÜ'],5))
+# def find_words(lst,n):
+#     new_list = []
+#     for i in lst:
+#         if len(i) > n:
+#             new_list.append(i)
+#     return new_list
+# print(find_words(['Manzoor','Hussain','OMU','ODTÜ'],5))
 
 
 #! Answer
