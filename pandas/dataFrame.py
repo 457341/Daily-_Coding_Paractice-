fbb07e718a59d713f710298e02f083c3622f0132
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-10-14 23:57:20 +0300

    String to Date time

--- pandas/dataFrame.py
+++ pandas/dataFrame.py
@@ -80,4 +80,14 @@ print(d.head())
 # dropping second row
 d.drop(d.index[[1,3]],inplace=True)
 print(d.head())
-#
+# converting string to date
+import pandas as pd
+import numpy as np
+s = pd.Series(['3/11/2000', '3/12/2000', '3/13/2000'])
+print("String Date:")
+print(s)
+r = pd.to_datetime(pd.Series(s))
+df = pd.DataFrame(r)
+print("Original DataFrame (string to datetime):")
+print(df)
+
