1b42818a951258a152746011196dac4d21e55e63
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-10-16 23:58:09 +0300

    Working with Pandas

--- pandas/dataFrame.py
+++ pandas/dataFrame.py
@@ -98,3 +98,13 @@ print(d.columns)
 dd = d.loc[:, d.columns != 'col3']
 print(dd)
 
+
+df = pd.DataFrame({
+    'name': ['Alberto Franco','Gino Mcneill','Ryan Parkes', 'Eesha Hinton', 'Syed Wharton', 'Kierra Gentry'],
+      'age': [18, 22, 85, 50, 80, 5]
+})
+print("Original DataFrame:")
+print(df)
+print('\nAge group:')
+df["age_groups"] = pd.cut(df["age"], bins = [0, 18, 65, 99], labels = ["kids", "adult", "elderly"])
+print(df["age_groups"])
