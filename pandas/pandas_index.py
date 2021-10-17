3b65dc2ceb813c155a075ebb49f9a8533d9e3332
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-10-17 23:54:43 +0300

    Indexing in Pandas

---
+++ pandas/pandas_index.py
@@ -0,0 +1,17 @@
+import pandas as pd
+students = pd.DataFrame({
+    "Name":['Manzoor','Ali','Ahmed'],
+    "University":['Omu','Odtu','Eru'],
+    "Class":[1,2,4],
+    "City":['Istanbul','Ankara','Samsun'],
+    "roll_no":  ['S01','S02','S03']
+})
+ogrenci_no = ['a', 'b', 'c']
+print(students)
+students = students.set_index('roll_no') # Changing the index Number
+print()
+print(students)
+# students  = students.set_index('ogrenci_no') # Changing the index Number 
+# if we want to change the index number, it must be any column
+print()
+print(students)
