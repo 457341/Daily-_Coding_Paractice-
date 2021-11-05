ddd4c51257589cbd4c8dc1b176fba2fd45a88a80
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-11-05 22:40:35 +0300

    Combine the columns of two potentially differently-indexed DataFrames into a single result DataFrame.

--- pandas/joining_and_merging.py
+++ pandas/joining_and_merging.py
@@ -1,6 +1,16 @@
 import pandas as pd
-s1 = pd.Series([0, 1, 2, 3], name='col1')
-s2 = pd.Series([0, 1, 2, 3])
-s3 = pd.Series([0, 1, 4, 5], name='col3')
-df = pd.concat([s1, s2, s3], axis=1, keys=['column1', 'column2', 'column3'])
-print(df)
+data1 = pd.DataFrame({'A': ['A0', 'A1', 'A2'],
+                      'B': ['B0', 'B1', 'B2']},
+                     index=['K0', 'K1', 'K2'])
+
+data2 = pd.DataFrame({'C': ['C0', 'C2', 'C3'],
+                      'D': ['D0', 'D2', 'D3']},
+                     index=['K0', 'K2', 'K3'])
+ 
+print("Original DataFrames:")
+print(data1)
+print("--------------------")
+print(data2)
+print("\nMerged Data (Joining on index):")
+result = data1.join(data2)
+print(result)
