165a025a06bdf5240fd1aee35d16c0ba34915bcb
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-11-10 21:47:56 +0300

    Pandas: Datetime and is interchangeable with it in most cases

--- pandas/time_series.py
+++ pandas/time_series.py
@@ -1,16 +1,10 @@
-import datetime
-from datetime import datetime
-print("Datetime object for Jan 11 2012:")
-print(datetime(2012, 1, 11))
-print("\nSpecific date and time of 9:20 pm") 
-print(datetime(2011, 1, 11, 21, 20))
-print("\nLocal date and time:")
-print(datetime.now())
-print("\nA date without time: ")
-print(datetime.date(datetime(2012, 5, 22)))
-print("\nCurrent date:")
-print(datetime.now().date())
-print("\nTime from a datetime:")
-print(datetime.time(datetime(2012, 12, 15, 18, 12)))
-print("\nCurrent local time:") 
-print(datetime.now().time())
+import pandas as pd
+#from datetime import datetime
+print("\nA specific date using timestamp:")
+print(pd.Timestamp('2016-11-10'))
+print("\nDate and time using timestamp:")
+print(pd.Timestamp('2012-05-03 11:30'))
+print("\nA time adds in the current local date using timestamp:")
+print(pd.Timestamp('11:30'))
+print("\nCurrent date and time using timestamp:")
+print(pd.Timestamp("now"))
