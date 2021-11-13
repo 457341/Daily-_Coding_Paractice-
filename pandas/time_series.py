78d397eeba4215236a68ed0aab650a07efc0248a
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-11-13 23:26:17 +0300

    Time-series with two index labels and random values

--- pandas/time_series.py
+++ pandas/time_series.py
@@ -1,12 +1,10 @@
 import pandas as pd
+import numpy as np
 import datetime
 from datetime import datetime, date
-today = datetime(2012, 10, 30)
-print("Current date:", today)
-tomorrow = today + pd.Timedelta(days=1)
-print("Tomorrow:", tomorrow)
-yesterday = today - pd.Timedelta(days=1)
-print("Yesterday:", yesterday)
-date1 = datetime(2016, 8, 2)
-date2 = datetime(2016, 7, 19)
-print("\nDifference between two dates: ",(date1 - date2))
+dates = [datetime(2011, 9, 1), datetime(2011, 9, 2)]
+print("Time-series with two index labels:")
+time_series = pd.Series(np.random.randn(2), dates)
+print(time_series)
+print("\nType of the index:")
+print(type(time_series.index))
