ab93bd560a418b982045146a85ef0e3f5809b703
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-11-15 23:46:12 +0300

    Time series

--- pandas/time_series.py
+++ pandas/time_series.py
@@ -1,12 +1,5 @@
 import pandas as pd
-data = {\
-"year": [2002, 2003, 2015, 2018],
-"day_of_the_year": [250, 365, 1, 140]
-}
-df = pd.DataFrame(data)
-print("Original DataFrame:")
-print(df)
-df["combined"] = df["year"]*1000 + df["day_of_the_year"]
-df["date"] = pd.to_datetime(df["combined"], format = "%Y%j")
-print("\nNew DataFrame:")
+s = pd.date_range('2021-01-01', periods=12, freq='BM')
+df = pd.DataFrame(s, columns=['Date'])
+print('last working days of each month of a specific year:')
 print(df)
