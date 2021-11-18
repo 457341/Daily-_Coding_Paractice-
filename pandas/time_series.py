59e46e0decbc5b9817e6d91ce4599d64169a35c7
Manzoor Hussain <33984489+manzoorHusain@users.noreply.github.com>
2021-11-18 23:57:58 +0300

    Update time_series.py

--- pandas/time_series.py
+++ pandas/time_series.py
@@ -7,3 +7,4 @@ print("\nConvert integer or float epoch times to Timestamp and DatetimeIndex upt
 dates2 = pd.to_datetime([1249720105100, 1249720105200, 1249720105300,
                 1249720105400, 1249720105500], unit='ms')
 print(dates2)
+print(dates2)
