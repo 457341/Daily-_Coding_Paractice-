ce25d43c3b93c1af68524949be505aa650cd67a1
Manzoor Hussain <33984489+manzoorHusain@users.noreply.github.com>
2021-11-20 23:27:12 +0300

    Update time_series.py

--- pandas/time_series.py
+++ pandas/time_series.py
@@ -1,19 +1,5 @@
 import pandas as pd
-from pandas.tseries.offsets import *
-import datetime
-from datetime import datetime, date
-dt = datetime(2020, 1, 4)
-print("Specified date:")
-print(dt)
-print("\nOne business day from the said date:")
-obday = dt + BusinessDay()
-print(obday)
-print("\nTwo business days from the said date:")
-tbday = dt + 2 * BusinessDay()
-print(tbday)
-print("\nThree business days from the said date:")
-thbday = dt + 3 * BusinessDay()
-print(thbday)
-print("\nNext business month end from the said date:")
-nbday = dt + BMonthEnd()
-print(nbday)
+mtp = pd.Period('2021-11','M')
+print("Monthly time perid: ",mtp)
+print("\nList of names in the current local scope:")
+print(dir(mtp)) 
