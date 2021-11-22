329d393223a290c51f8cba196d0b9fb0db391fa1
Manzoor Hussain <33984489+manzoorHusain@users.noreply.github.com>
2021-11-22 23:47:51 +0300

    Update time_series.py

--- pandas/time_series.py
+++ pandas/time_series.py
@@ -1,11 +1,7 @@
 import pandas as pd
-import datetime
-from datetime import datetime, date
-sdt = datetime(2020, 1, 1)
-edt = datetime(2020, 12, 31)
-dateset = pd.period_range(sdt, edt, freq='M')
-print("All monthly boundaries of a given year:")
-print(dateset) 
-print("\nStart and end time for each period object in the said index:")
-for d in dateset: 
-    print ("{0} {1}".format(d.start_time, d.end_time)) 
+print("Sequences of fixed-frequency dates and time spans (1 H):\n")
+r1 = pd.date_range('2030-01-01', periods=10, freq='H')
+print(r1)
+print("\nSequences of fixed-frequency dates and time spans (3 H):\n")
+r2 = pd.date_range('2030-01-01', periods=10, freq='3H')
+print(r2)
