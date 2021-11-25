01f2a6537728e730a2214cce56033bbbf2696545
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-11-25 23:57:33 +0300

    find the location address of a specified latitude and longitude

--- Geopy-Package/first_program.py
+++ Geopy-Package/first_program.py
@@ -1,22 +1,32 @@
 from geopy.geocoders import Nominatim
 geolocator = Nominatim(user_agent="geoapiExercises")
-state1 = "Uttar Pradesh"
-print("State Name:",state1)
-location = geolocator.geocode(state1)
-print("State Name/Country Name: ")
-print(location.address)
-state2 = " Illinois"
-print("\nState Name:",state2)
-location = geolocator.geocode(state2)
-print("State Name/Country Name: ")
-print(location.address)
-state3 = "Normandy"
-print("\nState Name:",state3)
-location = geolocator.geocode(state3)
-print("State Name/Country Name: ")
-print(location.address) 
-state4 = "Jerusalem District"
-print("\nState Name:",state4)
-location = geolocator.geocode(state4)
-print("State Name/Country Name: ")
-print(location.address)
+lald = "47.470706, -99.704723"
+print("Latitude and Longitude:",lald)
+location = geolocator.geocode(lald)
+print("Location address of the said Latitude and Longitude:")
+print(location)
+lald = "34.05728435, -117.194132331602"
+print("\nLatitude and Longitude:",lald)
+location = geolocator.geocode(lald)
+print("Location address of the said Latitude and Longitude:")
+print(location)
+lald = "38.8976998, -77.0365534886228"
+print("\nLatitude and Longitude:",lald)
+location = geolocator.geocode(lald)
+print("Location address of the said Latitude and Longitude:")
+print(location)
+lald = "55.7558° N, 37.6173° E"
+print("\nLatitude and Longitude:",lald)
+location = geolocator.geocode(lald)
+print("Location address of the said Latitude and Longitude:")
+print(location)
+lald = "35.6762° N, 139.6503° E"
+print("\nLatitude and Longitude:",lald)
+location = geolocator.geocode(lald)
+print("Location address of the said Latitude and Longitude:")
+print(location)
+lald = "41.9185° N, 45.4777° E"
+print("\nLatitude and Longitude:",lald)
+location = geolocator.geocode(lald)
+print("Location address of the said Latitude and Longitude:")
+print(location)
