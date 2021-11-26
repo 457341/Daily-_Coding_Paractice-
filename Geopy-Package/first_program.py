1c259fb32860a402a400da0871000af5a1846779
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-11-26 23:02:40 +0300

    Calculate the distance between London and New York city

--- Geopy-Package/first_program.py
+++ Geopy-Package/first_program.py
@@ -1,32 +1,5 @@
-from geopy.geocoders import Nominatim
-geolocator = Nominatim(user_agent="geoapiExercises")
-lald = "47.470706, -99.704723"
-print("Latitude and Longitude:",lald)
-location = geolocator.geocode(lald)
-print("Location address of the said Latitude and Longitude:")
-print(location)
-lald = "34.05728435, -117.194132331602"
-print("\nLatitude and Longitude:",lald)
-location = geolocator.geocode(lald)
-print("Location address of the said Latitude and Longitude:")
-print(location)
-lald = "38.8976998, -77.0365534886228"
-print("\nLatitude and Longitude:",lald)
-location = geolocator.geocode(lald)
-print("Location address of the said Latitude and Longitude:")
-print(location)
-lald = "55.7558° N, 37.6173° E"
-print("\nLatitude and Longitude:",lald)
-location = geolocator.geocode(lald)
-print("Location address of the said Latitude and Longitude:")
-print(location)
-lald = "35.6762° N, 139.6503° E"
-print("\nLatitude and Longitude:",lald)
-location = geolocator.geocode(lald)
-print("Location address of the said Latitude and Longitude:")
-print(location)
-lald = "41.9185° N, 45.4777° E"
-print("\nLatitude and Longitude:",lald)
-location = geolocator.geocode(lald)
-print("Location address of the said Latitude and Longitude:")
-print(location)
+from geopy import distance
+london = ("51.5074° N, 0.1278° W")
+newyork = ("40.7128° N, 74.0060° W")
+print("Distance between London and New York city (in km):")
+print(distance.distance(london, newyork).km," kms")
