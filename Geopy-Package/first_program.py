21afcb45636d93e494915f4d928618bebdb1546b
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-11-27 23:55:45 +0300

    find the latitude and longitude of a given location using Nominatim API

--- Geopy-Package/first_program.py
+++ Geopy-Package/first_program.py
@@ -1,5 +1,17 @@
-from geopy import distance
-london = ("51.5074° N, 0.1278° W")
-newyork = ("40.7128° N, 74.0060° W")
-print("Distance between London and New York city (in km):")
-print(distance.distance(london, newyork).km," kms")
+from geopy.geocoders import Nominatim
+geolocator = Nominatim(user_agent="geoapiExercises")
+ladd1 = "27488 Stanford Avenue, North Dakota"
+print("Location address:",ladd1)
+location = geolocator.geocode(ladd1)
+print("Latitude and Longitude of the said address:")
+print((location.latitude, location.longitude))
+ladd2 = "380 New York St, Redlands, CA 92373"
+print("\nLocation address:",ladd2)
+location = geolocator.geocode(ladd2)
+print("Latitude and Longitude of the said address:")
+print((location.latitude, location.longitude))
+ladd3 = "1600 Pennsylvania Avenue NW"
+print("\nLocation address:",ladd3)
+location = geolocator.geocode(ladd3)
+print("Latitude and Longitude of the said address:")
+print((location.latitude, location.longitude))
