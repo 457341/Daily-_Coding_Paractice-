978c495c317e49f5608747e018980d1fbc51d5cc
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-11-29 23:57:02 +0300

    program to generate holidays between two dates

--- pandas/time_series.py
+++ pandas/time_series.py
@@ -1,7 +1,8 @@
 import pandas as pd
-print("Sequences of fixed-frequency dates and time spans (1 H):\n")
-r1 = pd.date_range('2030-01-01', periods=10, freq='H')
-print(r1)
-print("\nSequences of fixed-frequency dates and time spans (3 H):\n")
-r2 = pd.date_range('2030-01-01', periods=10, freq='3H')
-print(r2)
+from pandas.tseries.holiday import *
+sdt = datetime(2021, 1, 1)
+edt = datetime(2030, 12, 31)
+print("Holidays between 2021-01-01 and 2030-12-31 using the US federal holiday calendar.")
+cal = USFederalHolidayCalendar()
+for dt in cal.holidays(start=sdt, end=edt): 
+    print (dt)
