8250987113f151abb68e5b69983eba6750c6c03f
Manzoor Hussain <manzoorhussain4428@gmail.com>
2021-12-27 23:07:13 +0300

    Lambda examples

--- Part-II/lambda_ex.py
+++ Part-II/lambda_ex.py
@@ -146,3 +146,24 @@ print(texts)
 result = list(filter(lambda x: (x == "".join(reversed(x))), texts)) 
 print("\nList of palindromes:")
 print(result) 
+
+
+my_list = [21,13,19,3,11,5,18]
+my_list.sort()
+print(my_list)
+print(my_list[len(my_list)//2])
+print(list(range(10,1,-1)))
+print(list(reversed(range(1,10))))
+print(reversed(list(range(1,11))))
+print(list(reversed(range(1,11))))
+
+# Write a Python program to find the numbers of a given string and store them in a list, display the numbers which are bigger than the length of the list in sorted form. Use lambda function to solve the problem.
+
+str1 = "sdf 23 safs8 5 sdfsd8 sdfs 56 21sfs 20 5"
+print("Original string: ",str1)
+str_num=[i for i in str1.split(' ')]
+lenght=len(str_num)
+numbers=sorted([int(x) for x in str_num if x.isdigit()])
+print('Numbers in sorted form:')
+for i in ((filter(lambda x:x>lenght,numbers))):
+    print(i,end=' ')
