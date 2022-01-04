69083e776eded6439ed3ab5250d1ed3b7c7cb3f8
Manzoor Hussain <manzoorhussain4428@gmail.com>
2022-01-04 22:52:11 +0300

    sort a given mixed list of integers and strings using lambda.

--- Part-II/lambda_ex.py
+++ Part-II/lambda_ex.py
@@ -218,3 +218,14 @@ print("Original tuple values:")
 print(tuple_str)
 print("\nNew tuple values:")
 print(tuple_int_str(tuple_str))
+
+
+# Write a Python program to sort a given mixed list of integers and strings using lambda. Numbers must be sorted before strings.
+def sort_mixed_list(mixed_list):
+    mixed_list.sort(key=lambda e: (isinstance(e, str), e))
+    return mixed_list
+mixed_list = [19,'red',12,'green','blue', 10,'white','green',1]
+print("Original list:")
+print(mixed_list)
+print("\nSort the said  mixed list of integers and strings:")
+print(sort_mixed_list(mixed_list))
