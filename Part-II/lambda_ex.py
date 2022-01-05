69f74ea9292cf4536dce580e6462641a95cfabbc
Manzoor Hussain <33984489+manzoorHusain@users.noreply.github.com>
2022-01-05 23:14:15 +0300

    Update lambda_ex.py

--- Part-II/lambda_ex.py
+++ Part-II/lambda_ex.py
@@ -227,5 +227,5 @@ def sort_mixed_list(mixed_list):
 mixed_list = [19,'red',12,'green','blue', 10,'white','green',1]
 print("Original list:")
 print(mixed_list)
-print("\nSort the said  mixed list of integers and strings:")
+print("Sort the said  mixed list of integers and strings:")
 print(sort_mixed_list(mixed_list))
