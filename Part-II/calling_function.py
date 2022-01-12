b5ae948e1c87b6fb667940ab2a41658efe535ec9
Manzoor Hussain <33984489+manzoorHusain@users.noreply.github.com>
2022-01-12 23:56:20 +0300

    Update calling_function.py

--- Part-II/calling_function.py
+++ Part-II/calling_function.py
@@ -8,4 +8,3 @@ def delay(fn, ms, *args):
 print("Square root after specific miliseconds:") 
 print(delay(lambda x: math.sqrt(x), 100, 16))
 print(delay(lambda x: math.sqrt(x), 1000, 100))
-print(delay(lambda x: math.sqrt(x), 2000, 25100))
