0f6996963f6d2c0230a67f0f1824091873155e3b
Manzoor Hussain <manzoorhussain4428@gmail.com>
2022-02-01 23:56:53 +0300

    Example

--- Part-II/Exampl.py
+++ Part-II/Exampl.py
@@ -11,3 +11,10 @@ str = "JAVASCRIPT"
 print("\nOriginal string: ",str)
 print("Coded string: ",test(str))
 print("Coded string: ",test(str))
+
+
+print("Input the heights of eight buildings:")
+l = [int(input()) for i in range(8)]
+print("Heights of the top three buildings:")
+l = sorted(l)
+print(*l[:4:-1], sep='\n')
