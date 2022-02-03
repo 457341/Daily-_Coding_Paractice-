f896c81a344b3be04a4239ebdb1b8d2d17b140e4
Manzoor Hussain <manzoorhussain4428@gmail.com>
2022-02-03 23:54:02 +0300

    Example

--- Part-II/Exampl.py
+++ Part-II/Exampl.py
@@ -1,6 +1,6 @@
-Write a Python program to create a coded string from a given string, using specified formula.
+# Write a Python program to create a coded string from a given string, using specified formula.
 
-Replace all 'P' with '9', 'T' with '0', 'S' with '1', 'H' with '6' and 'A' with '8'
+# Replace all 'P' with '9', 'T' with '0', 'S' with '1', 'H' with '6' and 'A' with '8'
 
 def test(str):
 	return str.translate(str.maketrans('PTSHA', '90168'))
