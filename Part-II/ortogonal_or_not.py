fe4f99248cd6ec06d10865631f8a09705c6d10d2
Manzoor Hussain <33984489+manzoorHusain@users.noreply.github.com>
2022-02-16 23:52:03 +0300

    Update ortogonal_or_not.py

--- Part-II/ortogonal_or_not.py
+++ Part-II/ortogonal_or_not.py
@@ -2,7 +2,6 @@
 
 # Input:
 # xp,yp, xq, yq, xr, yr, xs and ys are -100 to 100 respectively and each value can be up to 5 digits after the decimal point It is given as a real number including the number of.
-# Output:
 # Output AB and CD are not orthogonal! or AB and CD are orthogonal!.
 
 
